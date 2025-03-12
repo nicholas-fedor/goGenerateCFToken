8445f37c8ab509fa3e2451dc7e2556aa0be6ec4e
Nick Fedor <71477161+nicholas-fedor@users.noreply.github.com>
2025-03-12 16:51:33 -0700

    refactor(main.go): implement cloudflare v4 sdk
    - Update and tidy Go mod and sum files.
    - Migrate imports to Cloudflare-Go v4.
    
    **Important** This migration is broken!
    - Pending Cloudflare to resolve missing attributes for shared.TokenPolicyPermissionGroupParam ID and Name, which are necessary for creating API tokens,

--- main.go
+++ main.go
@@ -7,7 +7,11 @@ import (
 	"os"
 	"strings"
 
-	"github.com/cloudflare/cloudflare-go"
+	"github.com/cloudflare/cloudflare-go/v4"
+	"github.com/cloudflare/cloudflare-go/v4/accounts"
+	"github.com/cloudflare/cloudflare-go/v4/option"
+	"github.com/cloudflare/cloudflare-go/v4/shared"
+	"github.com/cloudflare/cloudflare-go/v4/zones"
 	"github.com/joho/godotenv"
 )
 
@@ -32,15 +36,12 @@ func main() {
 	// Set token and zone from loaded env variables
 	token := os.Getenv("CLOUDFLARE_API_TOKEN")
 	if token == "" {
-		log.Fatalln("Missing API Token")
+		log.Fatalln("Missing API token")
 	}
 	zone := os.Getenv("ZONE")
 
 	// Create API client using the scoped API token
-	api, err := cloudflare.NewWithAPIToken(token)
-	if err != nil {
-		log.Fatal(err)
-	}
+	api := cloudflare.NewClient(option.WithAPIToken(token))
 
 	// Most API calls require a Context
 	ctx := context.Background()
@@ -56,21 +57,23 @@ func main() {
 }
 
 // Returns the Zone ID
-func getZoneID(zone string, api *cloudflare.API, ctx context.Context) (string, error) {
-	zones, err := api.ListZones(ctx, zone)
+func getZoneID(zone string, api *cloudflare.Client, ctx context.Context) (string, error) {
+	zones, err := api.Zones.List(ctx, zones.ZoneListParams{
+		Name: cloudflare.F(zone)},
+	)
 	if err != nil {
 		return "", err
 	}
-	if len(zones) > 1 {
+	if len(zones.Result) > 1 {
 		err = fmt.Errorf("more than one zone returned")
 		return "", err
 	}
 
-	return zones[0].ID, nil
+	return zones.Result[0].ID, nil
 }
 
 // Generates an API token
-func generateToken(serviceName string, zone string, api *cloudflare.API, ctx context.Context) (string, error) {
+func generateToken(serviceName string, zone string, api *cloudflare.Client, ctx context.Context) (string, error) {
 	// Get the Zone ID from the zone name
 	zoneID, err := getZoneID(zone, api, ctx)
 	if err != nil {
@@ -83,29 +86,28 @@ func generateToken(serviceName string, zone string, api *cloudflare.API, ctx con
 	// Output input values
 	fmt.Println("Generating API token:", tokenName)
 
-	// Specify API token to create
-	resources := make(map[string]interface{})
-	resources["com.cloudflare.api.account.zone."+zoneID] = "*"
-	tokenToCreate := cloudflare.APIToken{
-		Name: tokenName,
-		Policies: []cloudflare.APITokenPolicies{{
-			Effect:    "allow",
-			Resources: resources,
-			PermissionGroups: []cloudflare.APITokenPermissionGroups{
-				{
-					ID:   "c8fed203ed3043cba015a93ad1616f1f",
-					Name: "Zone Read",
-				},
-				{
-					ID:   "4755a26eedb94da69e1066d98aa820be",
-					Name: "DNS Write",
-				},
-			},
-		}},
+	// Specify the API token to create
+	tokenToCreate := accounts.TokenNewParams{
+		AccountID: cloudflare.F("eb78d65290b24279ba6f44721b3ea3c4"),
+		Name:      cloudflare.F("readonly token"),
+		Policies: cloudflare.F([]shared.TokenPolicyParam{{
+			Effect: cloudflare.F(shared.TokenPolicyEffectAllow),
+			// TODO: Pending Cloudflare to fix borked SDK to allow include the ID and Name attributes.
+			PermissionGroups: cloudflare.F([]shared.TokenPolicyPermissionGroupParam{{
+				// ID:   cloudflare.F("c8fed203ed3043cba015a93ad1616f1f"),
+				// Name: cloudflare.F("Zone Read"),
+			}, {
+				// ID:   cloudflare.F("4755a26eedb94da69e1066d98aa820be"),
+				// Name: cloudflare.F("DNS Write"),
+			}}),
+			Resources: cloudflare.F(map[string]string{
+				"com.cloudflare.api.account.zone." + zoneID: "*",
+			}),
+		}}),
 	}
 
 	// Send the request to the Cloudflare API
-	generatedToken, err := api.CreateAPIToken(ctx, tokenToCreate)
+	generatedToken, err := api.Accounts.Tokens.New(ctx, tokenToCreate)
 	if err != nil {
 		return "", err
 	}
